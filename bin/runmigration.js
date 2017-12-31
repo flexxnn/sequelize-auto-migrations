6192d650064c58f4f334b932dfaac8785bfb84ae
Lenin Alevski <alevski@freeagentsoftware.com>
2017-12-31 14:31:35 -0600

    logging migrations on sequelizeMeta as thedeceptio suggested

--- bin/runmigration.js
+++ bin/runmigration.js
@@ -7,20 +7,19 @@ const Async             = require("async");
 
 const migrate           = require("../lib/migrate");
 
+let migrationsDir = path.join(process.env.PWD, 'migrations'),
+    modelsDir     = path.join(process.env.PWD, 'models');
+
 const optionDefinitions = [
     { name: 'rev', alias: 'r', type: Number, description: 'Set migration revision (default: 0)', defaultValue: 0 },
     { name: 'pos', alias: 'p', type: Number, description: 'Run first migration at pos (default: 0)', defaultValue: 0 },
     { name: 'one', type: Boolean, description: 'Do not run next migrations', defaultValue: false },
     { name: 'list', alias: 'l', type: Boolean, description: 'Show migration file list (without execution)', defaultValue: false },
-    { name: 'migrations-path', type: String, description: 'The path to the migrations folder' },
-    { name: 'models-path', type: String, description: 'The path to the models folder' },
     { name: 'help', type: Boolean, description: 'Show this message' }
 ];
 
 const options = commandLineArgs(optionDefinitions);
 
-let migrationsDir = path.join(process.env.PWD, options['migrations-path'] || 'migrations'),
-    modelsDir     = path.join(process.env.PWD, options['models-path'] || 'models');
 
 if (options.help)
 {
@@ -41,6 +40,27 @@ let fromRevision = options.rev;
 let fromPos = parseInt(options.pos);
 let stop = options.one;
 
+var tableName = 'SequelizeMeta' ;
+var SequelizeMeta = sequelize.define(tableName, {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+        autoIncrement: false
+    }
+});
+
+sequelize.query("CREATE TABLE `SequelizeMeta` (   `name` varchar(255) NOT NULL,   PRIMARY KEY (`name`),   UNIQUE KEY `name` (`name`),  UNIQUE KEY `SequelizeMeta_name_unique` (`name`) )");
+
+var already_uploaded = [];
+try {
+    sequelize.query("SELECT * FROM `SequelizeMeta`", { type: sequelize.QueryTypes.SELECT })
+        .then(users => {
+       for(var key in users){
+           already_uploaded[users[key].name] = true ;
+       }
+
 let migrationFiles = fs.readdirSync(migrationsDir)
 // filter JS files
   .filter((file) => {
@@ -58,9 +78,17 @@ let migrationFiles = fs.readdirSync(migrationsDir)
   .filter((file) => {
       let rev = parseInt( path.basename(file).split('-',2)[0]);
       return (rev >= fromRevision);
-  });
-  
-console.log("Migrations to execute:");  
+      //return false ;
+  })
+
+.filter((file) => {
+        return already_uploaded[file] != true ;
+}) ;
+// remove all migations which have already been loaded
+
+
+
+console.log("Migrations to execute:");
 migrationFiles.forEach((file) => {
     console.log("\t"+file);
 });
@@ -69,13 +97,17 @@ if (options.list)
     process.exit(0);
 
 
-Async.eachSeries(migrationFiles, 
+Async.eachSeries(migrationFiles,
     function (file, cb) {
         console.log("Execute migration from file: "+file);
         migrate.executeMigration(queryInterface, path.join(migrationsDir, file), fromPos, (err) => {
             if (stop)
                 return cb("Stopped");
-                
+            if(!err){
+                sequelize.query("INSERT INTO SequelizeMeta (name ) VALUES ( ? )",
+                     { replacements: [file] }).spread((results, metadata) => {
+                });
+            }
             cb(err);
         });
         // set pos to 0 for next migration
@@ -86,3 +118,8 @@ Async.eachSeries(migrationFiles,
         process.exit(0);
     }
 );
+
+});
+} catch (e){
+    console.log(e);
+}

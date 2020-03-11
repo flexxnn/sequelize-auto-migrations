c7dea6bc68e3c3a4131ba09e3273d668bdd36da8
naddyson <naddyson777@gmail.com>
2020-03-12 01:29:06 +0200

    fix property path in (fix no field info: outdated property names by moda20)

--- bin/makemigration.js
+++ bin/makemigration.js
@@ -69,7 +69,7 @@ try {
 } catch (e) { }
 
 //console.log(path.join(migrationsDir, '_current.json'), JSON.parse(fs.readFileSync(path.join(migrationsDir, '_current.json') )))
-let sequelize = require(modelsDir).database;
+let sequelize = require(modelsDir);
 
 let models = sequelize.models;
 
